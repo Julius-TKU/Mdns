c28f93d62881d0cc105746e9d9b915a08f560ad4
Julius-TKU <58931735+Julius-TKU@users.noreply.github.com>
2020-05-14 04:00:14 +0800

    Add files via upload

---
+++ precache-manifest.640870f04e1d923d6d3f8ae7ff6db2ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5fc3cbb3e707e665a0d6fa8622ef17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93127d8697350f91633a",
+    "url": "/static/css/2.ca49d6f0.chunk.css"
+  },
+  {
+    "revision": "119d26c3902ae217c440",
+    "url": "/static/css/main.66a14fb7.chunk.css"
+  },
+  {
+    "revision": "93127d8697350f91633a",
+    "url": "/static/js/2.4e3f6ac0.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.4e3f6ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119d26c3902ae217c440",
+    "url": "/static/js/main.dab9895a.chunk.js"
+  },
+  {
+    "revision": "0bfd521f937650725db6",
+    "url": "/static/js/runtime-main.72754eab.js"
+  }
+]);
